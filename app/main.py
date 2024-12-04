0d02a200af2508f1f583dfc00e9fef32780a7af2
Dmitriy-75 <dimakasimov29@gmail.com>
2024-12-04 10:24:56 +0300

    Module_17.iml

---
+++ app/main.py
@@ -0,0 +1,14 @@
+# В файле main.py создайте сущность FastAPI(), напишите один маршрут для неё - '/', по которому функция возвращает словарь
+# - {"message": "Welcome to Taskmanager"}.
+# Импортируйте объекты APIRouter и подключите к ранее созданному приложению FastAPI, объединив все маршруты в одно приложение.
+from fastapi import FastAPI, APIRouter
+from routers import task
+
+app=FastAPI()
+@app.get("/")
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
+
+app.include_router(task.router)
+
+# python -m uvicorn main:app
